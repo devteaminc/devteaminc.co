114c57b13dfdfeffb41b685ddbd70c63fcb23e2c
Mike Mitchell <mike.mitchell@liquidshop.com>
2014-02-21 10:14:04 +0000

    Deploy Test

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,16 @@
  * Load our dependencies
  */
 
-var gulp = require( 'gulp' );
-var sass = require( 'gulp-sass' );
-var autoprefixer = require( 'gulp-autoprefixer' );
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
-var imagemin = require( 'gulp-imagemin' );
+var imagemin = require('gulp-imagemin');
 var gulpConcat = require('gulp-concat');
-var notify = require( 'gulp-notify' );
+var uglify = require('gulp-uglify');
+var rev = require('gulp-rev');
+var usemin = require('gulp-usemin');
+var notify = require('gulp-notify');
 
 
 /**
@@ -28,16 +31,69 @@ var notify = require( 'gulp-notify' );
  */
 
 // Set paths for files
+
 var path = {
+
+	// Build Paths
+
 	build: {
+		root: './build',
 		css: './build/css',
 		img: './build/images',
 		js: './build/scripts'
 	},
+
+	// Development Paths
+
 	dev: {
+		root: './development',
 		css: './development/css',
 		sass: './development/sass',
 		img: './development/images',
 		js: './development/scripts'
 	}
+
 };
+
+
+/**
+ * Gulp Task
+ * =========
+ * Compile Sass to CSS
+ */
+
+gulp.task( 'compileSass', function () {
+
+	gulp.src( path.dev.sass + '/*.scss' )
+		.pipe( sass() )
+		.pipe( gulp.dest( path.dev.css ) )
+	;
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Scripts
+ */
+
+gulp.task( 'usemin', function () {
+
+	gulp.src('./development/*.html')
+		.pipe( usemin({
+			js: [ rev() ]
+		}))
+		.pipe( gulp.dest( path.build.root ))
+	;
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Default Task
+ */
+
+gulp.task( 'default', [ 'usemin' ] );
