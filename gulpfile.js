d98b8ab2488228423c4964a2935163c9550aa64d
Mike Mitchell <mike.mitchell@liquidshop.com>
2014-02-21 15:16:53 +0000

    Gulpfile fiddlin'

--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,12 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
-var gulpConcat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rev = require('gulp-rev');
 var usemin = require('gulp-usemin');
-var clean = require('gulp-clean');
+var rimraf = require('gulp-rimraf');
 var notify = require('gulp-notify');
+var runSequence = require('run-sequence');
 
 
 /**
@@ -60,33 +60,108 @@ var path = {
 /**
  * Gulp Task
  * =========
- * Build
+ * Clean up our build directory
+ */
+
+gulp.task( 'buildClean', function () {
+
+	return gulp.src( path.build.root + '/*', { read: false } )
+		.pipe( rimraf() )
+	;
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Compile Sass to CSS, auto-prefix and minify for build
+ */
+
+gulp.task( 'buildStyles', function () {
+
+	return gulp.src( path.dev.sass + '/*.scss' )
+		.pipe( sass() )
+		.pipe( autoprefixer( 'last 2 version' ))
+		.pipe( minifycss() )
+		.pipe( gulp.dest( path.dev.css ))
+	;
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Help prevent caching issues, update paths, minify JS
+ */
+
+gulp.task( 'buildUsemin', function () {
+
+	return gulp.src( path.dev.root + '/*.html' )
+		.pipe( usemin({
+			js: [ uglify(), rev() ],
+			css: [ rev() ],
+			options: {
+				assetsDirs: [ path.dev.root ]
+			}
+		}))
+		.pipe( gulp.dest( path.build.root ))
+	;
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Optimize Images
+ */
+
+gulp.task( 'buildImages', function () {
+
+	return gulp.src( path.dev.img + '/*' )
+		.pipe( imagemin() )
+		.pipe( rev() )
+		.pipe( gulp.dest( path.build.img ))
+	;
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * File Watcher
+ */
+
+gulp.task( 'watch', function () {
+
+	// Watch for changes to Sass files
+
+	gulp.watch( path.dev.sass + '/*', function(){
+
+		// Compile Sass to CSS
+
+		gulp.src( path.dev.sass +'/*.scss' )
+			.pipe( sass() )
+			.pipe( gulp.dest( path.dev.css ) )
+		;
+
+	});
+
+});
+
+
+/**
+ * Gulp Task
+ * =========
+ * Build Task
  */
 
 gulp.task( 'build', function () {
 
-	gulp.src( path.build.root, { read: false } )
-		.pipe( clean() )
-	;
-
-	// Compile Sass to CSS, add in vendor prefixes and minify
-
-	gulp.src( path.dev.sass + '/*.scss' )
-		.pipe( sass() )
-		.pipe( autoprefixer( 'last 2 version' ))
-		.pipe( minifycss() )
-		.pipe( gulp.dest( path.build.css ))
-	;
-
-	// Concat JavaScript, minify JavaScript and do some cache busting.
-
-	gulp.src('./development/*.html')
-		.pipe( usemin({
-			js: [ uglify(), rev() ],
-			css: [ rev() ]
-		}))
-		.pipe( gulp.dest( path.build.root ))
-	;
+	runSequence( 'buildClean', 'buildStyles', 'buildImages', 'buildUsemin' );
 
 });
 
